c89bbfa2b6cb295f7d26f1417d503c1f4ff63847
Nelis Santiago <143308422+nelisantiago@users.noreply.github.com>
2024-02-04 14:41:12 -0300

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <p style={{textAlign: "center", fontSize: 18}}>Voce e incrivel e merece todo respeito e apoio do mundo, parabens por se esta pessoal tão especial e transforma eu e os demais em um desenvolvedor melhor</p>
+  return (
+    <p 
+      style={{
+        textAlign: "center", 
+        fontSize: 18 
+      }}
+    >
+        Voce e incrivel e merece todo respeito e apoio do mundo, parabens por se esta pessoal tão especial e transforma eu e os demais em um desenvolvedor melhor
+    </p>
+  )
 }
