6db7a060a53219910291a590137d266272b3fc75
Nelis Santiago <143308422+nelisantiago@users.noreply.github.com>
2024-02-04 21:18:06 -0300

    newline aplicado ao arquivo nvmrc

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
         fontSize: 18 
       }}
     >
-        Voce e incrivel e merece todo respeito e apoio do mundo, parabens por se esta pessoal tão especial e transforma eu e os demais em um desenvolvedor melhor
+        Hello world
     </p>
   )
 }
