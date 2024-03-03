fe15c610f20ca22b31a7f242099eaecb349c774c
Nelis Santiago <143308422+nelisantiago@users.noreply.github.com>
2024-03-02 23:00:26 -0300

    adding new directory with endpoint status using method GET

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,6 @@
+export default function status(request, response) {
+  response.status(200).json({ 
+    status: 'ok',
+    version: '1.0.0'
+  })
+}
---
+++ tests/integration/api/v1/status/get.test.js
@@ -0,0 +1,7 @@
+const URL = "http://localhost:3000/api/v1/status"
+
+test("GET: /api/v1/status", async () => {
+  const response = await fetch(URL)
+  
+  expect(response.status).toBe(200)
+})
