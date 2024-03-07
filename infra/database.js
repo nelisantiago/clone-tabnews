f0c7b1b1fcf56dd8badb9a8506267f3bc2e64afe
Nelis Santiago <143308422+nelisantiago@users.noreply.github.com>
2024-03-06 23:56:16 -0300

    applied better engineering to the flow of environment variables

--- infra/database.js
+++ infra/database.js
@@ -1,8 +1,16 @@
 import { Client } from 'pg'
 
-const client = new Client(process.env.DATABASE_URL)
-  
+const config = {
+  user: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
+  host: process.env.POSTGRES_HOST,
+}
+
 async function query(query) {
+  const client = new Client(config)
+  
   await client.connect()
   const response = await client.query(query)
   await client.end()
